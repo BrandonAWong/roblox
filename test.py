1c1e7070993ad004e780f9b762c2639dd8f70797
BrandonAWong <105482178+BrandonAWong@users.noreply.github.com>
2023-01-16 19:32:52 -0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from PIL import ImageGrab
-import os
-def getTaskOnScreen(x1, y1, x2, y2):
-    screen = ImageGrab.grab(bbox = (x1, y1, x2, y2))
-    screen.save(f'{os.getcwd()}\\imgs\\screen\\task.png')
-
-getTaskOnScreen(925, 90, 990, 160)
